6dec67ec3ac387760ce7ae0d8445c128982dae49
Riku Pepponen <riku.pepponen@gmail.com>
2017-04-24 20:37:40 +0300

    Quit button works, New Game does not...

--- app/src/main/java/com/example/riku/xtremetictactoe/GameOverDialog.java
+++ app/src/main/java/com/example/riku/xtremetictactoe/GameOverDialog.java
@@ -7,6 +7,7 @@ package com.example.riku.xtremetictactoe;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 
@@ -18,8 +19,20 @@ public class GameOverDialog extends DialogFragment {
         return new AlertDialog.Builder(getActivity())
                 .setTitle(R.string.game_over)
                 .setMessage("Game over, dude!")
-                .setPositiveButton(R.string.new_game, null)
-                .setNegativeButton(R.string.quit, null)
+                .setPositiveButton(R.string.quit,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                getActivity().finish();
+                                System.exit(0);
+                        }
+                    })
+                .setNegativeButton(R.string.new_game,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                GameLogic.initalizeGame();
+                                dialog.dismiss();
+                            }
+                        })
                 .create();
     }
 }
--- app/src/main/java/com/example/riku/xtremetictactoe/StartScreenFragment.java
+++ app/src/main/java/com/example/riku/xtremetictactoe/StartScreenFragment.java
@@ -7,7 +7,6 @@ package com.example.riku.xtremetictactoe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
